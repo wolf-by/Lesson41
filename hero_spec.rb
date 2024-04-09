6199e312152986a1663da71b3fad00e5836e49b7
wolf-by <wolf.by@yandex.ru>
2024-04-09 17:54:05 +0300

    Add test for power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -6,4 +6,10 @@ describe Hero do
 
     expect(hero.name).to eq 'Mike' # hero.name == 'Mike'  
   end
+
+  it "can power up" do 
+    hero = Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110    
+  end
 end
