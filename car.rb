69623a785db28a4a77ef8ae0d30b0c84cd9323d6
wolf-by <wolf.by@yandex.ru>
2024-04-12 17:59:09 +0300

    Fixes #1

--- car.rb
+++ car.rb
@@ -12,10 +12,10 @@ class Car
   end
 
   def range
-    0
+    @fuel * MILES_PER_GALLON
   end  
 end
 
-#car = Car.new
-#car.add_fuel 10
-#puts "Range is #{car.range}"
+car = Car.new
+car.add_fuel 10
+puts "Range is #{car.range}"
